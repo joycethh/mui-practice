099a47510e28064e2643c1a441ca36925a732efd
joycethh <joycethh@outlook.com>
2022-09-19 15:41:54 -0500

    made sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,54 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+  Switch,
+} from "@mui/material";
+import {
+  Home,
+  Article,
+  Storefront,
+  Event,
+  Plagiarism,
+  Group,
+  Settings,
+  Brightness4,
+} from "@mui/icons-material";
+
+const lists = [
+  { text: "Homepage", icon: Home, id: 1 },
+  { text: "News Feed", icon: Article, id: 2 },
+  { text: "Market", icon: Storefront, id: 3 },
+  { text: "Event", icon: Event, id: 4 },
+  { text: "Pages", icon: Plagiarism, id: 5 },
+  { text: "Groups", icon: Group, id: 6 },
+  { text: "Setting", icon: Settings, id: 7 },
+];
 
 const Sidebar = () => {
   return (
-    <Box
-      backgroundColor="skyblue"
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Sidebar
-    </Box>
+    <div>
+      <List>
+        {lists.map((element) => (
+          <ListItem disablePadding key={element.id}>
+            <ListItemButton component="a" href={element.text}>
+              <ListItemIcon>{<element.icon />}</ListItemIcon>
+              <ListItemText primary={element.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+
+        <ListItemButton>
+          <ListItemIcon>
+            <Brightness4 />
+          </ListItemIcon>
+          <Switch defaultChecked />
+        </ListItemButton>
+      </List>
+    </div>
   );
 };
 
