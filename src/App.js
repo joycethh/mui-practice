a0c13f359e2ccc41b0c5744433b9a16ce20eb25d
joycethh <joycethh@outlook.com>
2022-09-19 12:31:45 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Button, styled } from "@mui/material";
 
 function App() {
-  const MyButton = styled(Button)({
-    backgroundColor: "red",
+  const MyButton = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.main,
     color: "#888",
     margin: 5,
     "&:hover": {
       backgroundColor: "blue",
     },
-  });
+  }));
   return (
     <div>
+      <Button variant="contained">secondary</Button>
+      <Button variant="contained" color="secondary">
+        secondary
+      </Button>
       <MyButton>one</MyButton>
       <MyButton>two</MyButton>
     </div>
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#009688",
+      light: "#e0f2f1",
+    },
+    secondary: {
+      main: "#e57373",
+      light: "#ffebee",
+    },
+  },
+});
