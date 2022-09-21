aeb8e4bf510eec5414c9d35c53858e4ae89a4f2e
joycethh <joycethh@outlook.com>
2022-09-21 16:27:18 -0500

    provide the theme in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import { useState } from "react";
 import Rightbar from "./components/Rightbar";
 import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
+import { DarkTheme, LightTheme } from "./theme";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-    </Box>
+    <ThemeProvider theme={isDarkMode ? DarkTheme : LightTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Box>
+    </ThemeProvider>
   );
 }
 
