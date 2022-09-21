93cbe367312c9e023107c8982fd107376721a113
joycethh <joycethh@outlook.com>
2022-09-21 15:05:27 -0500

    add function

--- src/components/Add.js
+++ src/components/Add.js
@@ -8,45 +8,22 @@ import {
   styled,
   TextField,
   Avatar,
-  Stack,
-  ListItemIcon,
-  ListItemText,
-  ListItemButton,
-  List,
-  IconButton,
 } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import {
-  EmojiEmotions,
-  KeyboardArrowUp,
-  AddPhotoAlternate,
-  PhotoCameraFront,
-  EditLocationAlt,
-} from "@mui/icons-material";
+
+import { Add as AddIcon } from "@mui/icons-material";
+import ComposeMenu from "./ComposeMenu";
 
 const Add = () => {
   const [openModal, setOpenModal] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const openMenu = Boolean(anchorEl);
-
   const handleOpenModal = () => setOpenModal(true);
   const handleCloseModal = () => setOpenModal(false);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleCloseMenu = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <>
       <Tooltip title="Add">
-        <IconButton onClick={handleOpenModal}>
-          <Fab size="small">
-            <AddIcon />
-          </Fab>
-        </IconButton>
+        <Fab size="small" onClick={handleOpenModal}>
+          <AddIcon />
+        </Fab>
       </Tooltip>
 
       <StyledModal open={openModal} onClose={handleCloseModal}>
@@ -54,9 +31,9 @@ const Add = () => {
           width={400}
           height={200}
           bgcolor="background.paper"
-          color={"text.primary"}
+          color="text.primary"
           p={3}
-          borderRadius={5}
+          borderRadius={2}
         >
           <Typography variant="h6" component="h2" textAlign="center">
             Create Post
@@ -74,36 +51,7 @@ const Add = () => {
             variant="standard"
             placeholder="Share your thoughts"
           />
-
-          <Box
-            sx={{ display: "flex", justifyContent: "center", padding: 1.5 }}
-            aria-controls={openMenu ? "basic-menu" : undefined}
-          >
-            <IconButton onClick={handleClick}>
-              <KeyboardArrowUp />
-            </IconButton>
-          </Box>
-
-          <Stack direction="row" spacing={1}>
-            <List
-              id="basic-menu"
-              sx={{
-                width: "100%",
-                maxWidth: 400,
-                bgcolor: "background.paper",
-              }}
-              component="menu"
-              open={openMenu}
-              onClose={handleCloseMenu}
-            >
-              {menuData.map((item) => (
-                <ListItemButton key={item.id}>
-                  <ListItemIcon>{<item.icon />}</ListItemIcon>
-                  <ListItemText primary={item.text} />
-                </ListItemButton>
-              ))}
-            </List>
-          </Stack>
+          <ComposeMenu />
         </Box>
       </StyledModal>
     </>
@@ -121,28 +69,5 @@ const PostBox = styled(Box)({
   alignItems: "center",
   gap: 10,
 });
-
-const menuData = [
-  {
-    id: 1,
-    icon: EmojiEmotions,
-    text: "Feelings",
-  },
-  {
-    id: 2,
-    icon: AddPhotoAlternate,
-    text: "Image",
-  },
-  {
-    id: 3,
-    icon: PhotoCameraFront,
-    text: "Videos",
-  },
-  {
-    id: 4,
-    icon: EditLocationAlt,
-    text: "Location",
-  },
-];
 
 export default Add;
