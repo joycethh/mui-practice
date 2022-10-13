dc77e7fa000ba58c50e94ccaccb0e94a7b18b01d
joycethh <joycethh@outlook.com>
2022-10-13 13:01:39 -0500

    NEW. create api index

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:5000/",
+});
+
+export const fetchPosts = () => API.get("/posts");
+export const getPost = (id) => API.get(`/posts/${id}`);
+export const createPost = (newPost) => API.post("/posts", newPost);
+export const deletePost = (id) => API.delete(`'/posts/${id}`);
