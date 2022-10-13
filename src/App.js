e36c7520493dcc0c92abf8997b23ea79a09f0a11
joycethh <joycethh@outlook.com>
2022-10-13 16:51:57 -0500

    use the fetch all action in the feed page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Rightbar from "./components/rightbar/Rightbar";
-import Home from "./components/pages/Home";
+import NewsFeed from "./components/posts/NewsFeed";
 import Sidebar from "./components/sidebar/Sidebar";
 import { Stack, ThemeProvider, CssBaseline } from "@mui/material";
 import Navbar from "./components/navbar/Navbar";
@@ -19,7 +19,7 @@ function App() {
         sx={{ paddingLeft: 20, paddingRight: 20 }}
       >
         <Sidebar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-        <Home />
+        <NewsFeed />
         <Rightbar />
       </Stack>
     </ThemeProvider>
--- src/components/posts/NewsFeed.js
+++ src/components/posts/NewsFeed.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import Post from "./Post";
 import Create from "../compose/Create";
+
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "../../actions/postsAction";
+
 const NewsFeed = () => {
+  const [id, setId] = useState(0);
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [posts, dispatch]);
+
   return (
     <Box flex={4} p={2}>
-      <Create />
-      <Post />
+      <Create id={id} setId={setId} />
+      {posts &&
+        posts.length > 0 &&
+        posts.map((post) => (
+          <div key={post._id}>
+            <Post id={id} post={post} />
+          </div>
+        ))}
     </Box>
   );
 };
