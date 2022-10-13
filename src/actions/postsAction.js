cb33c38673f539f56081d1a0c3ebb866bd04cf0e
joycethh <joycethh@outlook.com>
2022-10-13 13:45:54 -0500

    create posts action

---
+++ src/actions/postsAction.js
@@ -0,0 +1,42 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../contants/actionType";
+import * as api from "../api/index";
+
+export const fetchPosts = () => async (dispatch) => {
+  const { data } = await api.fetchPosts();
+  console.log("action fetchall-data", data);
+  try {
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPost = (newPost) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(newPost);
+    console.log("action create-data", data);
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+    console.log("action update-data", data);
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
