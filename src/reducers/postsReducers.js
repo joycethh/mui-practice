635b977cb8a0cc74f57f8992b2e0fe1970fa0d32
joycethh <joycethh@outlook.com>
2022-10-13 16:50:56 -0500

    change the initialvalue to be posts array

--- src/reducers/postsReducers.js
+++ src/reducers/postsReducers.js
@@ -1,26 +1,49 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionType";
 
-export const postsReducer = (state = {}, action) => {
+// export const postsReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case FETCH_ALL:
+//       return {
+//         posts: action.payload,
+//       };
+//     case CREATE:
+//       return {
+//         posts: [...state.posts, action.payload],
+//       };
+//     case UPDATE:
+//       return {
+//         posts: [action.payload, ...state.posts],
+//       };
+//     case DELETE:
+//       return {
+//         posts: state.posts.filter(
+//           (restPosts) => restPosts._id !== action.payload
+//         ),
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+export const postsReducer = (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
-      return {
-        posts: action.payload,
-      };
+      return action.payload;
+
     case CREATE:
-      return {
-        posts: [...state.posts, action.payload],
-      };
+      return [action.payload, ...posts];
     case UPDATE:
-      return {
-        posts: [action.payload, ...state.posts],
-      };
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
     case DELETE:
-      return {
-        posts: state.posts.filter(
-          (restPosts) => restPosts._id !== action.payload
-        ),
-      };
+      return posts.filter((post) => post._id !== action.payload);
     default:
-      return state;
+      return posts;
   }
 };
+// FETCH_ALL_START
+// isPostsLoading: true
+
+// FETCH_ALL_SUCCESS
+// isPostsLoading: false
