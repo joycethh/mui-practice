053e694acc278ade5c483424ac4de0ab081903bf
joycethh <joycethh@outlook.com>
2022-10-13 12:38:39 -0500

    dispatch action the component

--- src/components/compose/Create.js
+++ src/components/compose/Create.js
@@ -1,35 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Box, TextField, styled, Button, Card, Stack } from "@mui/material";
 
 import { InsertPhoto, Tag } from "@mui/icons-material/";
+import { CreatePost } from "../../services";
 
 const Create = () => {
+  const dispatch = useDispatch();
+  const [postData, setPostData] = useState({
+    // author:"",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPostData({ ...postData, [name]: value });
+  };
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    dispatch(
+      CreatePost({
+        ...postData,
+      })
+    );
+    console.log("dispatch", dispatch(CreatePost));
+  };
+
   return (
     <>
       <Card sx={{ marginLeft: 2, marginRight: 2 }}>
-        <InputBox>
-          <TextField
-            label="what is your thought?"
-            variant="filled"
-            multiline
-            fullWidth
-            rows={2}
-            InputProps={{
-              disableUnderline: true,
-            }}
-          />
-        </InputBox>
-        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-          <StyledStack direction="row" spacing={2}>
-            <InsertPhoto sx={{ color: "#ff7f31" }} />
-            <Tag sx={{ color: "#ff7f31" }} />
-          </StyledStack>
-          <BtnWrapper>
-            <StyledButton variant="contained" color="secondary" size="small">
-              Submit
-            </StyledButton>
-          </BtnWrapper>
-        </Box>
+        <form autoComplete="off" noValidate onSubmit={handleSumbit}>
+          <InputBox>
+            <TextField
+              name="message"
+              label="what is your thought?"
+              variant="filled"
+              multiline
+              fullWidth
+              rows={2}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              onChange={handleChange}
+            />
+          </InputBox>
+
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <StyledStack direction="row" spacing={2}>
+              <InsertPhoto sx={{ color: "#ff7f31" }} />
+              <Tag sx={{ color: "#ff7f31" }} />
+            </StyledStack>
+            <BtnWrapper>
+              <StyledButton
+                variant="contained"
+                color="secondary"
+                size="small"
+                onClick={handleSumbit}
+              >
+                Submit
+              </StyledButton>
+            </BtnWrapper>
+          </Box>
+        </form>
       </Card>
     </>
   );
@@ -49,7 +84,6 @@ const StyledButton = styled(Button)(({ theme }) => ({
   textTransform: "capitalize",
   color: "#fff",
   justifyContent: "center",
-
   fontSize: 12,
   fontWeight: 600,
 }));
