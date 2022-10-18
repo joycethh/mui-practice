fe27d40e34a3bf21ad4f5ef31e4b756aa2bf8d73
joycethh <joycethh@outlook.com>
2022-10-18 13:32:15 -0500

    create Home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 import Rightbar from "../rightbar/Rightbar";
 import NewsFeed from "../posts/NewsFeed";
@@ -7,15 +7,18 @@ import Sidebar from "../sidebar/Sidebar";
 const Home = ({ isDarkMode, setIsDarkMode }) => {
   return (
     <>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        sx={{ paddingLeft: 20, paddingRight: 20 }}
+      <Box
+        sx={{
+          paddingLeft: { lg: 18, md: 10 },
+          paddingRight: { lg: 18, md: 5 },
+        }}
       >
-        <Sidebar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-        <NewsFeed />
-        <Rightbar />
-      </Stack>
+        <Stack direction="row" justifyContent="center" spacing={1}>
+          <Sidebar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+          <NewsFeed />
+          <Rightbar />
+        </Stack>
+      </Box>
     </>
   );
 };
