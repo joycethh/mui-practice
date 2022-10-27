af5419feaf733e4390e729e3d8fcd5a19d0dd6f2
joycethh <joycethh@outlook.com>
2022-10-27 14:31:22 -0500

    change the endpoint for likepost functon

--- src/api/index.js
+++ src/api/index.js
@@ -11,5 +11,4 @@ export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) =>
   API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
-export const likePost = (id, updatedPost) =>
-  API.patch(`/posts/${id}/likePost`, updatedPost);
+export const likePost = (id) => API.patch(`/posts/${id}`);
