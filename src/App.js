d082fed8135a92bbec2ed4b07da236f528752b6d
joycethh <joycethh@outlook.com>
2022-11-01 11:16:29 -0500

    install google oauth and wrap the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import { useState } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
@@ -6,28 +8,29 @@ import { DarkTheme, LightTheme } from "./theme";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import PostDetails from "./components/posts/PostDetails";
-import UpdatePost from "./components/posts/UpdatePost";
+const ID = process.env.REACT_APP_GOOGLE_ID;
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={isDarkMode ? DarkTheme : LightTheme}>
-        <CssBaseline />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/posts" />} />
-          <Route
-            path="/posts"
-            element={
-              <Home isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-            }
-          />
-          <Route path="/posts/:id" element={<PostDetails />} />
-          {/* <Route path="/posts/:id/edit" element={<UpdatePost />} /> */}
-        </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={ID}>
+      <BrowserRouter>
+        <ThemeProvider theme={isDarkMode ? DarkTheme : LightTheme}>
+          <CssBaseline />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/posts" />} />
+            <Route
+              path="/posts"
+              element={
+                <Home isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+              }
+            />
+            <Route path="/posts/:id" element={<PostDetails />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   );
 }
 
