9517b20b1c2740edb7452a4d4aa09770173db5cd
joycethh <joycethh@outlook.com>
2022-11-08 12:00:09 -0600

    use good old redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { createStore, applyMiddleware, compose } from "redux";
+import { reducers } from "./reducers";
+import thunk from "redux-thunk";
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
