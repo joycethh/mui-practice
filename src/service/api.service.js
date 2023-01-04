acdbc3c4976e6dc70e2fdb445c42dc30841a72e7
joycethh <joycethh@outlook.com>
2023-01-04 16:20:45 -0600

    update url

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: "https://funget-social.herokuapp.com/",
 });
 
 API.interceptors.request.use((req) => {
